21a54807995cd7a53bbe6d18271a6525cf85d5cb
Thiago Andrade <thiago.irias.64@gmail.com>
2026-04-05 20:15:39 -0300

    Add files via upload

---
+++ cinco_num.py
@@ -0,0 +1,7 @@
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+num3 = int(input('Digite o terceiro número: '))
+num4 = int(input('Digite o quarto número: '))
+num5 = int(input('Digite o quinto número: '))
+
+print(f'{num1} \n{num2} \n{num3} \n{num4} \n{num5}')
